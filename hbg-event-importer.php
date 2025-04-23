7bb664c8ca21b53a345db2e6ae5a3f791b31f603
Adam Alexandersson <adam.alexandersson@consid.se>
2025-04-23 11:45:27 +0200

    Set version to 1.4.3

--- hbg-event-importer.php
+++ hbg-event-importer.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Event Manager
  * Plugin URI:        http://github.com/helsingborg-stad/api-event-manager/
  * Description:       Manage events locally, and import from XCAP & CBIS.
- * Version:           1.0.0
+ * Version:           1.4.3
  * Author:            Kristoffer Svanmark, Sebastian Thulin, Tommy Morberg, Jonatan Hanson
  * Author URI:        http://www.helsingborg.se
  * License:           MIT
